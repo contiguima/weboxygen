a4c5e187681b9cdb7ddeadb3f3a4136995ad200d
contiguima <contiguima@gmail.com>
2023-06-05 21:42:32 -0300

    Comienzo Layout

--- oxygen-web/src/app/pages/MainPage.js
+++ oxygen-web/src/app/pages/MainPage.js
@@ -1,11 +1,23 @@
 import Navbar from "../components/Navbar";
 import "../styles/mainpage.css"
 
+
 const MainPage = () => {
     return (<>
         <Navbar/>
         <section className="layoutHome">
             <h1>Invertí a favor del ambiente</h1>
+            <h3>Protegemos árboles de ser talados y conservamos  <br/> territorios nativos</h3>
+            <button >Comenzá</button>
+
+            <div className="dataBoard">
+                <p><strong>133.150</strong> hectareas de bosque</p>
+                <div className="dataBoardLine"></div>
+                <p><strong>133.150</strong> hectareas de bosque</p>
+                <div className="dataBoardLine"></div>
+                <p><strong>133.150</strong> hectareas de bosque</p>
+                <div className="dataBoardLine"></div>
+            </div>
         </section>
 
         
